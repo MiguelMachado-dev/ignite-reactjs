2c25e5d64825026e089d4471a02ea54762635a09
Miguel Machado <miiguellmp@gmail.com>
2022-07-28 11:57:07 -0300

    feat: sidebar component

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,15 +1,22 @@
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
 import { Post } from "./Post";
 
-import "./global.css"
+import "./global.css";
+import S from "./App.module.css";
 
 export function App() {
   return (
     <>
       <Header />
 
-      <Post author="John Doe" content="This is John Doe!" />
-      <Post author="Miguel Machado" content="This is Miguel Machado!" />
+      <div className={S.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="John Doe" content="This is John Doe!" />
+          <Post author="Miguel Machado" content="This is Miguel Machado!" />
+        </main>
+      </div>
     </>
   );
 }
---
+++ 01-fundamentos-reactjs/src/components/Sidebar/index.jsx
@@ -0,0 +1,21 @@
+import S from "./styles.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={S.sidebar}>
+      <img
+        className={S.cover}
+        src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
+
+      <div className={S.profile}>
+        <strong>Miguel Machado</strong>
+        <span>Frontend Engineer</span>
+      </div>
+
+      <footer>
+        <a href="#">Editar seu perfil</a>
+      </footer>
+    </aside>
+  );
+}
