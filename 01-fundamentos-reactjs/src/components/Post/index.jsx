edc0d6ad0428da0df910b3897d6cfe091d22d339
Miguel Machado <miiguellmp@gmail.com>
2022-07-28 14:55:56 -0300

    feat: comment form component

--- 01-fundamentos-reactjs/src/components/Post/index.jsx
+++ 01-fundamentos-reactjs/src/components/Post/index.jsx
@@ -31,11 +31,20 @@ export function Post() {
           👉 <a href="#">jane.design/doctorcare</a>
         </p>
         <p>
-          <a href="#">#novoprojeto</a>{' '}
-          <a href="#">#nlw</a>{' '}
+          <a href="#">#novoprojeto</a> <a href="#">#nlw</a>{" "}
           <a href="#">#rocketseat</a>
         </p>
       </div>
+
+      <form className={S.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea placeholder="Deixe um comentário" />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   );
 }
