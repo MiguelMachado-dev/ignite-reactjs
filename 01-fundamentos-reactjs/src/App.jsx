cb35fc8e3c6474a681d868a5a6a7257e9485e3bb
Miguel Machado <miiguellmp@gmail.com>
2022-07-28 11:07:11 -0300

    feat: add css modules support

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,8 +1,13 @@
+import { Header } from "./components/Header";
 import { Post } from "./Post";
+
+import "./styles.css"
 
 export function App() {
   return (
     <>
+      <Header />
+
       <Post author="John Doe" content="This is John Doe!" />
       <Post author="Miguel Machado" content="This is Miguel Machado!" />
     </>
---
+++ 01-fundamentos-reactjs/src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+import S from "./styles.module.css";
+
+export function Header() {
+  return (
+    <header className={S.header}>
+      <strong>Ignite feed</strong>
+    </header>
+  );
+}
