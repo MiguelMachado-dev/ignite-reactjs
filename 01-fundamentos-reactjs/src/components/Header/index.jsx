1fd83e25974785d5392cd686429ec315c37408e7
Miguel Machado <miiguellmp@gmail.com>
2022-07-28 11:34:30 -0300

    feat: header component

--- 01-fundamentos-reactjs/src/components/Header/index.jsx
+++ 01-fundamentos-reactjs/src/components/Header/index.jsx
@@ -3,6 +3,7 @@ import S from "./styles.module.css";
 export function Header() {
   return (
     <header className={S.header}>
+      <img src="/ignite-logo.svg" alt="Logotipo do Ignite" />
       <strong>Ignite feed</strong>
     </header>
   );
