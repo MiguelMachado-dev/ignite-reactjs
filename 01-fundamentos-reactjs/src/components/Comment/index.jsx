399d564f795ab0e1d860eeb1f68829bfa653eeff
Miguel Machado <miiguellmp@gmail.com>
2022-12-06 15:19:23 -0300

    feat: comment like

--- 01-fundamentos-reactjs/src/components/Comment/index.jsx
+++ 01-fundamentos-reactjs/src/components/Comment/index.jsx
@@ -1,9 +1,14 @@
 import { ThumbsUp, Trash } from "phosphor-react";
+import { useState } from "react";
 import { Avatar } from "../Avatar";
 import S from "./styles.module.css";
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
   const handleDeleteComment = () => onDeleteComment(content);
+
+  const handleLikeComment = () => setLikeCount(likeCount + 1);
 
   return (
     <div className={S.comment}>
@@ -31,9 +36,9 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
